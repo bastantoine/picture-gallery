123a0838efe5e5bf1270e750671d25b8fca728e5
bastantoine <baba_cmoi@hotmail.fr>
2020-04-25 17:08:58 +0200

    Added models for Album and Picture

--- django_api/api/models.py
+++ django_api/api/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+class Album(models.Model):
+
+    name = models.CharField(max_length=100)
+    description = models.TextField(null=True, blank=True)
+    start_date = models.DateField()
+    end_date = models.DateField(null=True, blank=True)
+    parent_album = models.ForeignKey('self',
+                                     null=True,
+                                     blank=True,
+                                     on_delete=models.CASCADE
+                                    )
+
+    def __str__(self):
+        return self.name
+
+
+class Picture(models.Model):
+
+    path = models.ImageField()
+    album = models.ForeignKey('Album', on_delete=models.CASCADE)
