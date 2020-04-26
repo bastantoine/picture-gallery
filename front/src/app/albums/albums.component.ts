3320f8140d8465e6ab95b19c2dfc39a34f29fc3f
bastantoine <baba_cmoi@hotmail.fr>
2020-04-26 10:13:43 +0200

    Update AlbumsComponents to display albums

--- front/src/app/albums/albums.component.ts
+++ front/src/app/albums/albums.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Album } from "../models";
+
 @Component({
   selector: 'app-albums',
   templateUrl: './albums.component.html',
@@ -7,9 +9,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AlbumsComponent implements OnInit {
 
+  albums: Album[] = [];
+
   constructor() { }
 
   ngOnInit(): void {
+    let description = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.';
+    for (let i = 1; i <= 30; i++) {
+      this.albums.push(
+        new Album(
+          i, `Album ${i}`, new Date(2020, 4, 26),
+          i%5 == 0 ? description : ''
+        )
+      )
+    }
   }
 
 }
