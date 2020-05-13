724ac0ade6de88616b2ac0e238f758a3e70fc4f8
bastantoine <baba_cmoi@hotmail.fr>
2020-05-13 15:12:07 +0200

    Use AlbumService to get the list of albums and display them

--- front/src/app/albums/albums.component.ts
+++ front/src/app/albums/albums.component.ts
@@ -12,16 +12,35 @@ import { AlbumsService } from "../albums.service";
 })
 export class AlbumsComponent implements OnInit {
 
-  albums: Observable<Album[]>;
+  albums: Album[];
 
   constructor(private albumsService: AlbumsService) { }
 
   ngOnInit(): void {
+    this.albums = [];
     this.getAllAlbums();
   }
 
   getAllAlbums(): void {
-    this.albums = this.albumsService.getAllAlbums();
+    this.albumsService.getAllAlbums()
+      .subscribe(
+        albums => {
+          albums.forEach(album => this.albums.push(
+            new Album(
+              album['id'],
+              album['name'],
+              album['url'],
+              new Date(album['start_date']),
+              album['description'],
+              new Date(album['end_date']),
+              )))
+            console.log(this.albums[0].constructor.name);
+        },
+        err => console.error('There has been an error: ' + err),
+        () => {
+          console.log('Complete');
+        }
+      );
   }
 
 }
