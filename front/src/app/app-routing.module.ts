e3557673effaba34b6b3e3d7f0cf3ae4b188d5e8
bastantoine <baba_cmoi@hotmail.fr>
2020-05-15 16:50:56 +0200

    Added routing and routed AlbumsComponent to base route

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { AlbumsComponent } from "./albums/albums.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: AlbumsComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
