930d73d22a4e3572807d679cb893735a820cdf80
bastantoine <baba_cmoi@hotmail.fr>
2020-05-15 17:34:32 +0200

    Routed AlbumDetailComponent to /album/:id

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { AlbumsComponent } from "./albums/albums.component";
+import { AlbumDetailComponent } from "./album-detail/album-detail.component";
 
 const routes: Routes = [
   { path: 'home', component: AlbumsComponent },
   { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'album/:id', component: AlbumDetailComponent }
 ];
 
 @NgModule({
