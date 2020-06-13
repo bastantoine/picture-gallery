d81d8e5499eb25602ef252eb0c4b2ea7faf9c7af
bastantoine <baba_cmoi@hotmail.fr>
2020-06-13 23:08:57 +0200

    Added API to extract the most useful EXIF tags of a picture

--- django_api/api/urls.py
+++ django_api/api/urls.py
@@ -1,11 +1,17 @@
+from django.urls import path
 from rest_framework import routers
 
 from api.views import (
     AlbumViewSet,
-    PictureViewSet
+    PictureViewSet,
+    ExifsView
 )
 
 router = routers.SimpleRouter()
 router.register(r'albums', AlbumViewSet)
 router.register(r'pictures', PictureViewSet)
 urlpatterns = router.urls
+
+urlpatterns += [
+    path('exifs/<int:id_picture>', ExifsView.as_view()),
+]
