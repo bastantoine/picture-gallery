5859e4ed39ff2f3b1152c257b2db93f169327a4f
bastantoine <baba_cmoi@hotmail.fr>
2020-06-13 19:23:24 +0200

    Display the picture in the picture component

--- front/src/app/picture/picture.component.ts
+++ front/src/app/picture/picture.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from "@angular/router";
+
+import { PictureService } from "../pictures.service";
+import { Picture } from "../models";
 
 @Component({
   selector: 'app-picture',
@@ -7,9 +11,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PictureComponent implements OnInit {
 
-  constructor() { }
+  picture: Picture;
+
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private pictureService: PictureService
+  ) { }
 
   ngOnInit(): void {
+    this.picture = new Picture(0, '', 0);
+    this.getPicture();
+  }
+
+  getPicture(): void {
+    let id = +this.route.snapshot.paramMap.get('id');
+    this.pictureService.getPictureById(id)
+      .subscribe(
+        picture => this.picture = new Picture(
+          picture['id'],
+          picture['path'],
+          picture['album']
+        ),
+        () => this.router.navigate(['/not-found'])
+      );
   }
 
 }
