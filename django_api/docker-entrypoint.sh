be59062bd896ecce63815340afe6aa35eb1dc285
bastantoine <bastien.antoine69+github@gmail.com>
2020-08-10 19:07:21 +0200

    Added entrypoint script for the API container

---
+++ django_api/docker-entrypoint.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# From https://stackoverflow.com/questions/33992867/how-do-you-perform-django-database-migrations-when-using-docker-compose
+
+# Apply database migrations
+echo "Collect database migrations"
+python manage.py makemigrations
+
+# Apply database migrations for the API
+echo "Collect database migrations"
+python manage.py makemigrations api
+
+# Apply database migrations
+echo "Apply database migrations"
+python manage.py migrate
+
+# Start server
+echo "Starting server"
+python manage.py runserver 0.0.0.0:8080
